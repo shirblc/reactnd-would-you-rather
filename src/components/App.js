26667bebaabd11a4be2c99b06b6b3b1bec9be464
Shir Bar Lev <shirblc@gmail.com>
2020-11-23 14:38:03 +0000

    Added getInitialData calls to App component upon mounting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import './App.css';
+import { getInitialData as getQuestions } from '../actions/questions';
+import { getInitialData as getUsers } from '../actions/users';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      Would You Rather
-    </div>
-  );
+class App extends React.Component {
+	componentDidMount() {
+		this.props.dispatch(getQuestions());
+		this.props.dispatch(getUsers());
+	}
+	
+	render() {
+		 return (
+			 <div className="App">
+				 Would You Rather
+			 </div>
+		 );
+	}
 }
 
-export default App;
+export default connect()(App);
