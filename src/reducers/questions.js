86822cfab2171e259f280351dc86b8d7d77b8da3
Shir Bar Lev <shirblc@gmail.com>
2020-11-23 13:03:22 +0000

    Added questions reducer.

---
+++ src/reducers/questions.js
@@ -0,0 +1,13 @@
+import { RECEIVE_DATA } from '../actions/questions';
+
+// Questions reducer
+export default function questionsReducer(state = {}, action) {
+	switch(action.type) {
+		// if the action is getting new questions data, add them
+		case RECEIVE_DATA:
+			return { ...state, action.questions }
+		// otherwise return the state
+		default:
+			return state
+	}
+}
