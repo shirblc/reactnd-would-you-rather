2cc8a421ee32cd1ecf4f9034e3a01c97644f3b3e
Shir Bar Lev <shirblc@gmail.com>
2020-11-23 13:14:06 +0000

    Added missing spread operators to reducers.

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -5,7 +5,7 @@ export default function questionsReducer(state = {}, action) {
 	switch(action.type) {
 		// if the action is getting new questions data, add them
 		case RECEIVE_DATA:
-			return { ...state, action.questions }
+			return { ...state, ...action.questions }
 		// otherwise return the state
 		default:
 			return state
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -5,7 +5,7 @@ export default function usersReducer(state = {}, action) {
 	switch(action.type) {
 		// if the action is getting new users data, add them
 		case RECEIVE_DATA:
-			return { ...state, action.users }
+			return { ...state, ...action.users }
 		// otherwise return the state
 		default:
 			return state
