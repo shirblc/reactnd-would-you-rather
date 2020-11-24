cb9caca9a740aed42bda09e112f5f6780a59df33
Shir Bar Lev <shirblc@gmail.com>
2020-11-24 14:45:54 +0000

    Updated the reducers with the new action.

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DATA } from '../actions/questions';
+import { RECEIVE_DATA, UPDATE_QUESTION } from '../actions/questions';
 
 // Questions reducer
 export default function questionsReducer(state = {}, action) {
@@ -6,6 +6,9 @@ export default function questionsReducer(state = {}, action) {
 		// if the action is getting new questions data, add them
 		case RECEIVE_DATA:
 			return { ...state, ...action.questions }
+		// if the action is answering a question, update the question's data
+		case UPDATE_QUESTION:
+			return { ...state, ...action.question }
 		// otherwise return the state
 		default:
 			return state
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DATA } from '../actions/users';
+import { RECEIVE_DATA, UPDATE_USER } from '../actions/users';
 
 // Users reducer
 export default function usersReducer(state = {}, action) {
@@ -6,6 +6,9 @@ export default function usersReducer(state = {}, action) {
 		// if the action is getting new users data, add them
 		case RECEIVE_DATA:
 			return { ...state, ...action.users }
+		// if the action is answering a question, update the user's data
+		case UPDATE_USER:
+			return { ...state, ...action.user }
 		// otherwise return the state
 		default:
 			return state
