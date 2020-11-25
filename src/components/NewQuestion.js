752733d4d58be4061e86883cce789b321763c931
Shir Bar Lev <shirblc@gmail.com>
2020-11-25 13:12:35 +0000

    Added state update in New Question component.

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -7,14 +7,24 @@ class NewQuestion extends React.Component {
 		optionTwo: ''
 	};
 	
+	// update the answer option as currently held by the component's state
+	updateOptionText(newText, textBox) {
+		// get the option to update; the ID of the text field minus the 'text' in the end
+		const optionToUpdate = textBox.substring(0, textBox.length - 4);
+		// set the state
+		this.setState((currentState) => ({
+			currentState[optionToUpdate]: newText
+		}))
+	}
+	
 	// render method
 	render() {
 		return (
 			<form>
 				<label for='optionOneText'>Option one:</label>
-				<input type='text' id='optionOneText' placeholder='option one' value={this.state.optionOne} />
+				<input type='text' id='optionOneText' placeholder='option one' value={this.state.optionOne} onChange={(e) => ( this.updateOptionText(e.target.value) )} />
 				<label for='optionTwoText'>Option one:</label>
-				<input type='text' id='optionTwoText' placeholder='option two' value={this.state.optionTwo} />
+				<input type='text' id='optionTwoText' placeholder='option two' value={this.state.optionTwo} onChange={(e) => ( this.updateOptionText(e.target.value, e.target.id) )} />
 				<button type='submit'>Add Question</button>
 			</form>
 		)
