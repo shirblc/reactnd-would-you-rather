9869a3b573ba3b4174f0c7a053c3596e40b3aa68
Shir Bar Lev <shirblc@gmail.com>
2020-11-25 12:18:27 +0000

    Added logout action.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,9 +1,17 @@
 export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
 
 // Action creator for handling a user logging in
 export function login(id) {
 	return {
 		type: LOGIN,
 		id
+	}
+}
+
+// Action creator for handling user logout
+export function logout() {
+	return {
+		type: LOGOUT
 	}
 }
--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,4 +1,4 @@
-import { LOGIN } from '../actions/currentUser';
+import { LOGIN, LOGOUT } from '../actions/currentUser';
 
 // Current user reducer
 export default function currentUserReducer(state = null, action) {
@@ -6,6 +6,9 @@ export default function currentUserReducer(state = null, action) {
 		// if the action is a user login, change the state to the given user ID
 		case LOGIN:
 			return action.id
+		// if the action is a user logout, change the state back to null
+		case LOGOUT:
+			return null
 		// otherwise return the current state
 		default:
 			return state
