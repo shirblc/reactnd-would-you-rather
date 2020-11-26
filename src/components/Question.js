bd8e3ae51498fa082a54f0f7569122469f41d430
Shir Bar Lev <shirblc@gmail.com>
2020-11-26 13:04:49 +0000

    Added error handling in case a question doesn't exist.

--- src/components/Question.js
+++ src/components/Question.js
@@ -17,29 +17,38 @@ class Question extends React.Component {
 	// render
 	render() {
 		return (
-			<div id='questionContainer'>
-				<Link to='/'><FontAwesomeIcon icon='arrow-left'/></Link>
-				<img src={this.props.question.userPic} alt='user profile pic' className='profilePic' />
-				<div id='titleCont'>
-					<div>Asked:</div>
-					<h3>Would you rather...</h3>
-				</div>
-				{
-					Object.entries(this.props.question.options).map(entry => (
-						<div className='answer' key={entry[0]}>
-							<button id={entry[0]} disabled={this.props.answered === true} onClick={(e) => (this.answerQuestion(e.target.id))}>{ entry[1].text }</button>
-							{ entry[1].votes.includes(this.props.currentUser) &&
-							  <FontAwesomeIcon icon='check' className='selected' /> }
-							{ this.props.answered &&
-								<div className='stats'>
-									<div>Number of votes: { entry[1].votes.length }</div>
-									<div>{ (entry[1].votes.length / this.props.totalVotes * 100).toFixed(2) }% chose this answer.</div>
-								</div>
+			<React.Fragment>
+				{ this.props.question
+					? <div id='questionContainer'>
+							<Link to='/'><FontAwesomeIcon icon='arrow-left'/></Link>
+							<img src={this.props.question.userPic} alt='user profile pic' className='profilePic' />
+							<div id='titleCont'>
+								<div>Asked:</div>
+								<h3>Would you rather...</h3>
+							</div>
+							{
+								Object.entries(this.props.question.options).map(entry => (
+									<div className='answer' key={entry[0]}>
+										<button id={entry[0]} disabled={this.props.answered === true} onClick={(e) => (this.answerQuestion(e.target.id))}>{ entry[1].text }</button>
+										{ entry[1].votes.includes(this.props.currentUser) &&
+										  <FontAwesomeIcon icon='check' className='selected' /> }
+										{ this.props.answered &&
+											<div className='stats'>
+												<div>Number of votes: { entry[1].votes.length }</div>
+												<div>{ (entry[1].votes.length / this.props.totalVotes * 100).toFixed(2) }% chose this answer.</div>
+											</div>
+										}
+									</div>
+								))
 							}
 						</div>
-					))
+					: <div id='errorMessage'>
+							<h3>This question doesn't exist!</h3>
+							<Link to='/'>Go back to home page</Link>
+						</div>
 				}
-			</div>
+			</React.Fragment>
+			
 		)
 	}
 }
@@ -47,27 +56,26 @@ class Question extends React.Component {
 // Map State to Props
 // Get the details of the specific question
 function mapStateToProps({ questions, currentUser, users }, { match }) {
-	const originalQuestion = questions[match.params.id];
 	// move the question's potential answers to its own property; this is used by the template for more concise code
-	const quest = {
-		...originalQuestion,
-		userPic: users[originalQuestion.author].avatarURL,
-		options: {
-			optionOne: {
-			  ...originalQuestion.optionOne
-			},
-			optionTwo: {
-			  ...originalQuestion.optionTwo
+	const originalQuestion = questions[match.params.id] ? ({
+			...questions[match.params.id],
+			userPic: users[questions[match.params.id].author].avatarURL,
+			options: {
+				optionOne: {
+				  ...questions[match.params.id].optionOne
+				},
+				optionTwo: {
+				  ...questions[match.params.id].optionTwo
+				}
 			}
-		}
-	}
+		}) : null;
 	
 	return {
-		question: quest,
+		question: originalQuestion,
 		// checks whether the user answered the question
-		answered: originalQuestion.optionOne.votes.includes(currentUser) || originalQuestion.optionTwo.votes.includes(currentUser),
+		answered: originalQuestion ? originalQuestion.optionOne.votes.includes(currentUser) || originalQuestion.optionTwo.votes.includes(currentUser) : 0,
 		// checks how many votes there are in total for the question
-		totalVotes: originalQuestion.optionOne.votes.length + originalQuestion.optionTwo.votes.length,
+		totalVotes: originalQuestion ? originalQuestion.optionOne.votes.length + originalQuestion.optionTwo.votes.length : 0,
 		currentUser: currentUser
 	}
 }
